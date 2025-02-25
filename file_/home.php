d5e0494f225f763a35d5aafa59eb0ca28f852552
Jojoo18 <jonathanchristian838@gmail.com>
2025-02-25 12:12:13 +0700

    Resolve merge conflict

--- file_/home.php
+++ file_/home.php
@@ -1,3 +1,4 @@
+<?php include '_parcials/_template/header.php';?>
 <!DOCTYPE html>
 <html lang="id">
   <head>
@@ -10,31 +11,7 @@
   </head>
   <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container">
-        <a class="navbar-brand fw-bold" href="#"> <i class="bi bi-fingerprint text-warning"></i> GeeBot </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link text-light" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light" href="#">Contact</a>
-            </li>
-          </ul>
-          <a href="#" class="btn btn-warning fw-bolder ms-3" style="border-radius: 20px">Login <i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-    </nav>
+  
     <!-- Akhir Navbar -->
 
     <!-- Jumbotron -->
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 <?php
 $page = isset($_GET['page']) ? $_GET['page'] :"home";
@@ -14,4 +15,22 @@ function loadPage($page) : void {
     }
 }
 
+=======
+
+<?php
+$page = isset($_GET['page']) ? $_GET['page'] :"home";
+
+function loadPage($page) : void {
+    switch ($page){
+        case 'home':
+            include 'file_/home.php';
+            case 'login' :
+            include 'file_/login.php';
+            default :
+            echo "404 - Halaman tidak ditemukan";
+            break;
+    }
+}
+
+>>>>>>> 584b744 (update hari ini)
 loadPage(page: $page);?>
